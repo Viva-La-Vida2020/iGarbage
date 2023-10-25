b909236b19372d0b713cb6ed80b71edf3280bc69
pillarliang <pillarliang21@gmail.com>
2023-10-25 15:55:07 +0800

    add speech-recognition using openai-api

---
+++ speech-recognition/index.py
@@ -0,0 +1,61 @@
+import pyaudio  # 录音程序
+import wave
+import openai
+
+# 一次读取数据流的数据量，避免一次性的数据量大大
+CHUNK = 1024
+# 采样精度
+FORMAT = pyaudio.paInt16
+# 声道数
+CHANNELS = 1
+# 采样频率
+RATE = 11025
+# 录音时长，单位秒
+RECORD_SECONDS = 3
+#
+OUTPUT_FILENAME = 'output.wav'
+openai.api_key = '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'
+
+
+def transcribe(file: str):
+    audio_file = open(file, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    return transcript['text']
+
+
+def recorder(output_file: str, time=3):
+    # 初始化PyAudio
+    audio = pyaudio.PyAudio()
+
+    # 开始录制
+    print("Recording...")
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True,
+                        frames_per_buffer=CHUNK)
+
+    frames = []
+
+    for _ in range(0, int(RATE / CHUNK * time)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print("Finished recording")
+    # 停止录制
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    # 保存为WAV文件
+    with wave.open(output_file, 'wb') as wf:
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(audio.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
+
+    print(f"Saved as {output_file}")
+
+
+if __name__ == "__main__":
+    recorder(OUTPUT_FILENAME, RECORD_SECONDS)
+    result = transcribe(OUTPUT_FILENAME)
+    print(result)
